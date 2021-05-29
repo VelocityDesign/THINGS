fbbf61823f00381f71548f07cbc239717dc3211f
Judah B <me@jtechnologies.xyz>
2021-05-28 23:23:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Import Modules
-
-# -- NO MODULES :( --
+# following imports are required by PKI
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
 
 # Make the Identity class
 class Identity:
